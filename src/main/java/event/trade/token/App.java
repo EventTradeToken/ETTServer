50173b562231602d26593388f6a37d530fafc2c8
Gochan57 <gochan@mail.ru>
2018-06-03 22:36:04 +0300

    working tomcat jetty server

---
+++ src/main/java/event/trade/token/App.java
@@ -0,0 +1,13 @@
+package event.trade.token;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
